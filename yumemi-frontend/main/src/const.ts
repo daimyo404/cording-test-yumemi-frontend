313569b7f8f9ebd43a7dd763267328b44fcaf191
saito-hiroaki <hiro021312@gmail.com>
2022-08-13 23:19:12 +0900

    [mod]ディレクトリ構成の見直し

--- yumemi-frontend/main/src/const.ts
+++
@@ -1,6 +0,0 @@
-export const resasUrl = {
-    prefectures: "https://opendata.resas-portal.go.jp/api/v1/prefectures",
-    pupulationPerYear: "https://opendata.resas-portal.go.jp/api/v1/population/composition/perYear"
-}
-
-Object.freeze(resasUrl)
